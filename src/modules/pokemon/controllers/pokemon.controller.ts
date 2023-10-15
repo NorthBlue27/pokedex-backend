6e95f7bb29996664501fdef78e75c1426643a742
NorthBlue27 <joseph_gutierrez_27@outlook.es>
2023-10-15 09:16:22 -0500

    up2

--- src/modules/pokemon/controllers/pokemon.controller.ts
+++ src/modules/pokemon/controllers/pokemon.controller.ts
@@ -1,25 +1,38 @@
-import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { PokemonService } from '../services/pokemon.service';
 import { CreateDtoPokemon } from '../dto/create-pokemon.dto';
+import { UpdateDtoPokemon } from '../dto/update-pokemon.dto';
 
 @Controller('pokemon')
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
   @Post()
+  // @HttpCode(200)
+  // @HttpCode(HttpStatus.OK)
   create(@Body() createDtoPokemon: CreateDtoPokemon) {
     return this.pokemonService.create(createDtoPokemon);
+  }
+  @Get(':term')
+  findOne(@Param('term') term: string) {
+    return this.pokemonService.findOne(term);
   }
   @Get()
   findAll() {
     this.pokemonService.findAll();
   }
-  @Get()
-  findOne() {
-    this.pokemonService.findOne();
-  }
-  @Patch()
-  update() {
-    this.pokemonService.update();
+  @Patch(':term')
+  update(@Param('term') term: string, @Body() upadteDtoPokemon: UpdateDtoPokemon) {
+    return this.pokemonService.update(term, upadteDtoPokemon);
   }
   @Delete()
   remove() {
--- src/modules/pokemon/services/pokemon.service.ts
+++ src/modules/pokemon/services/pokemon.service.ts
@@ -2,11 +2,14 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateDtoPokemon } from '../dto/create-pokemon.dto';
 import { PokemonMg } from '../pokemon.entity';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { UpdateDtoPokemon } from '../dto/update-pokemon.dto';
+import { isInt, isString } from 'class-validator';
 
 @Injectable()
 export class PokemonService {
@@ -24,20 +27,38 @@ export class PokemonService {
         throw new BadRequestException(
           `Key duplicated ${JSON.stringify(error.keyValue)}`,
         );
-      else console.log(error);
+      console.log(error);
       throw new InternalServerErrorException(
         `Can't created pokemon - Check server logs`,
       );
     }
   }
-  findOne() {
-    return '';
+  async findOne(term: string) {
+    let pokemon: PokemonMg;
+
+    // Id
+    if (isInt(term)) pokemon = await this.pokemonModel.findOne({ no: term });
+    // MongoId
+    else if (isValidObjectId(term))
+    pokemon = await this.pokemonModel.findOne({ _id: term });
+    // Name
+    else if (isString(term))
+    pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase() });
+
+    if (!pokemon)
+      throw new NotFoundException(`Pokemon doesn't exist with ${term}`);
+
+    return pokemon;
   }
   findAll() {
     return '';
   }
-  update() {
-    return '';
+  async update(term: string, updateDtoPokemon: UpdateDtoPokemon) {
+    const pokemon = await this.findOne(term);
+    if (updateDtoPokemon.name)
+      updateDtoPokemon.name = updateDtoPokemon.name.toLowerCase();
+    await pokemon.updateOne(updateDtoPokemon, { new: true });
+    return { ...pokemon.toJSON(), ...updateDtoPokemon };
   }
   remove() {
     return '';
