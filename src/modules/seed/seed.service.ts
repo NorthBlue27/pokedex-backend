e68720b5ac448ad70140df9f418ca0fa5563ba4b
NorthBlue27 <joseph_gutierrez_27@outlook.es>
2023-10-15 22:24:28 -0500

    antes de insertar

--- src/modules/seed/seed.service.ts
+++ src/modules/seed/seed.service.ts
@@ -7,7 +7,7 @@ export class SeedService {
   private readonly axios: AxiosInstance = axios;
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit?=1',
+      'https://pokeapi.co/api/v2/pokemon?limit=1',
     );
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
